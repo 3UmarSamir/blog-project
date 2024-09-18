ec29364b4742bea8ff5474d32fd124e559a7124a
3Umar <osamir0099@gmail.com>
2024-09-18 06:32:24 +0300

    lfg,

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo chown -R ubuntu:ubuntu ~/financetracker
-virtualenv /home/ubuntu/financetracker/venv
-source /home/ubuntu/financetracker/venv/bin/activate
-pip install -r /home/ubuntu/financetracker/requirements.txt
+sudo chown -R ubuntu:ubuntu ~/blogprojectdrf
+virtualenv /home/ubuntu/blogprojectdrf/venv
+source /home/ubuntu/blogprojectdrf/venv/bin/activate
+pip install -r /home/ubuntu/blogprojectdrf/requirements.txt
